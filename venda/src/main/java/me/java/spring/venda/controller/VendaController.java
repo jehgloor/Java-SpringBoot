eadaa30a23acfef463dc6a5b1fd669b73ec0a4af
jehgloor <89281266+jehgloor@users.noreply.github.com>
2022-08-08 12:35:44 -0300

    adicionando o metedo PUT para atualizar por id

--- venda/src/main/java/me/java/spring/venda/controller/VendaController.java
+++ venda/src/main/java/me/java/spring/venda/controller/VendaController.java
@@ -12,11 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 import me.java.spring.venda.controller.dto.VendaDto;
+import me.java.spring.venda.controller.form.AtualizacaoVendaForm;
 import me.java.spring.venda.controller.form.VendaForm;
 import me.java.spring.venda.models.Venda;
 import me.java.spring.venda.repository.VendaRepository;
@@ -53,5 +55,12 @@ public class VendaController {
 		vendaRepository.save(venda);
 		URI uri = uriBuilder.path("/vendas/{id}").buildAndExpand(venda.getId()).toUri();
 		return ResponseEntity.created(uri).body(new VendaDto(venda));
+	}
+	
+	@Transactional
+	@PutMapping("/{id}")
+	public ResponseEntity<VendaDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoVendaForm form){
+		Venda venda = form.atualizar(id, vendaRepository);
+		return ResponseEntity.ok(new VendaDto(venda));
 	}
 }
---
+++ venda/src/main/java/me/java/spring/venda/controller/form/AtualizacaoVendaForm.java
@@ -0,0 +1,24 @@
+package me.java.spring.venda.controller.form;
+
+import javax.validation.constraints.Min;
+import me.java.spring.venda.models.Venda;
+import me.java.spring.venda.repository.VendaRepository;
+
+public class AtualizacaoVendaForm {
+	@Min(0)
+	private double valorVenda;
+	
+	public double getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(double valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public Venda atualizar(Long id, VendaRepository vendaRepository) {
+		Venda venda = vendaRepository.getReferenceById(id);
+		venda.setValorVenda(this.valorVenda);
+		return venda;
+	}
+}
